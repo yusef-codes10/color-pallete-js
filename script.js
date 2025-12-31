4ee4d2faec09b3c5d1848e33aa931c20fafd64ff
Youcef <youcefcopy@gmail.com>
2025-12-31 21:44:40 +0100

    adding js logic

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('js has been loaded');
+
+const btn = document.querySelector('button');
+const colorPallete = document.querySelector('.color-pallete');
+
+btn.addEventListener('click',
+    pickColor
+)
+
+function pickColor() {
+    colorPallete.classList.toggle('hidden');
+}
